50087ef3ad8ec0e6f21aed83e1c7339adc559d1e
Vuthy_Tourn <vuthytuon168@gmail.com>
2026-05-19 00:39:43 +0700

    Updated

--- product-service/src/main/java/dev/sample/productservice/ProductRestController.java
+++ product-service/src/main/java/dev/sample/productservice/ProductRestController.java
@@ -1,6 +1,7 @@
 package dev.sample.productservice;
 
 
+import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.UUID;
 
+@Hidden
 @RestController
 @RequestMapping("/api/v1/products")
 public class ProductRestController {
